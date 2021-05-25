cabf3e2aa21648d7ec8e5646d504f2ff3dea391b
Mayank Parida <68542629+mayankparida@users.noreply.github.com>
2021-05-25 21:00:11 +0530

    Implement Firebase Authentication

---
+++ lib/home/presentation/screen/allCountry/allCountry_screen.dart
@@ -0,0 +1,17 @@
+import 'package:covinfo/constants.dart';
+import 'package:flutter/material.dart';
+
+class AllCountryScreen extends StatelessWidget {
+  static String id = "allCountry_screen";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: gradient,
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:covinfo/authentication/presentation/screen/loading/loading_screen.dart';
 import 'package:covinfo/authentication/presentation/screen/login/login_screen.dart';
 import 'package:covinfo/authentication/presentation/screen/signup/signUp_screen.dart';
 import 'package:covinfo/authentication/presentation/screen/splash/splash_screen.dart';
+import 'package:covinfo/home/presentation/screen/allCountry/allCountry_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -12,11 +17,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: "Leela"),
-      initialRoute: SignUpScreen.id,
+      initialRoute: LoadingScreen.id,
       routes: {
         SplashScreen.id: (context) => SplashScreen(),
         LoginScreen.id: (context) => LoginScreen(),
         SignUpScreen.id: (context) => SignUpScreen(),
+        LoadingScreen.id: (context) => LoadingScreen(),
+        AllCountryScreen.id: (context) => AllCountryScreen(),
       },
     );
   }
